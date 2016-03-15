305e5a93f3fd4b5052c7efef83fb1928e55d3615
SilverBirchh <Bradley.Birch@ig.com>
2016-03-15 17:27:19 +0000

    Set up login. Need to implement method

--- app/controllers/members/login.js
+++ app/controllers/members/login.js
@@ -9,9 +9,15 @@ export default Ember.Controller.extend({
   validUsername: Ember.computed.gte('username.length', 5),
   validPassword: Ember.computed.match('password', /^[a-zA-Z]\w{3,14}$/),
 
-
   isValid: Ember.computed.and('validUsername', 'validPassword'),
 
   isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    login() {
+      // TODO: Set up this method.
+    }
+  }
 
 });
