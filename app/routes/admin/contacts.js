15fc9b2f579e4a3eff59df59fe9cecbe68e28121
SilverBirchh <Bradley.Birch@ig.com>
2016-03-17 10:53:07 +0000

    Delete functions

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -4,6 +4,17 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('feedback');
+  },
+
+  actions: {
+
+    deleteFeedback(feedback) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        feedback.destroyRecord();
+      }
+    }
   }
 
 
