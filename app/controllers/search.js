72a6fcc96039b55b3ad3533a8836827a9a9796cf
Bradley Birch <Bradley.Birch@ig.com>
2016-04-01 17:29:04 +0100

    Trying to sub to opne market

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -32,32 +32,52 @@ export default Ember.Controller.extend({
         async: false,
         mimeType: req.binary ? 'text/plain; charset=x-user-defined' : null
       }).then(function(response, status, data) {
-          that.get('results').clear();
+        that.get('results').clear();
 
-          for (var i = 0; i < response.markets.length; i++) {
-            var marketsData = response.markets[i];
-            marketsData.tidyEpic = marketsData.epic.replace(/\./g, "_");
-            marketsData.tidyExpiry = marketsData.expiry.replace(/ /g, "");
-            marketsData.linkIdEpic = "searchResult_" + marketsData.tidyEpic;
-            marketsData.linkIdState = "search_" + marketsData.tidyEpic + "_MARKET_STATE";
-            marketsData.linkIdBid = "search_" + marketsData.tidyEpic + "_BID";
-            marketsData.linkIdOffer = "search_" + marketsData.tidyEpic + "_OFFER";
-            marketsData.state = null;
-            if (marketsData.marketStatus === 'EDITS_ONLY') {
-              marketsData.state = 'assets/images/edit.png';
-            } else if (marketsData.marketStatus === 'TRADEABLE') {
-              marketsData.state = 'assets/images/open.png';
-            } else {
-              marketsData.state = 'assets/images/close.png';
-            }
-            if (that.get('results').length < 39) {
-              that.get('results').addObject(marketsData);
-            }
+        for (var i = 0; i < response.markets.length; i++) {
+          var marketsData = response.markets[i];
+          marketsData.tidyEpic = marketsData.epic.replace(/\./g, "_");
+          marketsData.tidyExpiry = marketsData.expiry.replace(/ /g, "");
+          marketsData.linkIdEpic = "searchResult_" + marketsData.tidyEpic;
+          marketsData.linkIdState = "search_" + marketsData.tidyEpic + "_MARKET_STATE";
+          marketsData.linkIdBid = "search_" + marketsData.tidyEpic + "_BID";
+          marketsData.linkIdOffer = "search_" + marketsData.tidyEpic + "_OFFER";
+          marketsData.state = null;
+          if (marketsData.marketStatus === 'EDITS_ONLY') {
+            marketsData.state = 'assets/images/edit.png';
+          } else if (marketsData.marketStatus === 'TRADEABLE') {
+            marketsData.state = 'assets/images/open.png';
+          } else {
+            marketsData.state = 'assets/images/close.png';
           }
-        },
-        function(e) {
-          console.log(e.responseText);
-        });
+          if (that.get('results').length < 39) {
+            that.get('results').addObject(marketsData);
+          }
+        }
+        var subscription = new Lightstreamer.Subscription(
+          "MERGE", ["MARKET:IX.D.FTSE.DAILY.IP", "MARKET:MT.D.GC.MONTH1.IP"], // e.g. {"MARKET:IX.D.FTSE.DAILY.IP","MARKET:MT.D.GC.MONTH1.IP"}
+          ["BID", "OFFER"] // e.g. {"BID", "OFFER"}
+        );
+        subscription.addListener({
+          onSubscription: function() {
+            console.log('subscribed');
+          },
+          onUnsubscription: function() {
+            console.log('unsubscribed');
+          },
+          onSubscriptionError: function(code, message) {
+            console.log('subscription failure: ' + code + " message: " + message);
+          },
+          onItemUpdate: function(updateInfo) {
+            var epic = updateInfo.getItemName().split(":")[1];
+            updateInfo.forEachField(function(fieldName, fieldPos, value) {
+              console.log('Field: ' + fieldName + " Value: " + value);
+              var confirm = JSON.parse(value);
+              console.log('json: ' + confirm.dealId)
+            });
+          }
+        });  lsClient.subscribe(subscription);
+      });
     }
   }
 });
