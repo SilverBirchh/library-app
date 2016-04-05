8b13bab6c6667f04b8ab69a441fcb2a7947fc2a9
Bradley Birch <Bradley.Birch@ig.com>
2016-04-05 12:07:34 +0100

    Getting account details

--- app/controllers/search/account.js
+++ app/controllers/search/account.js
@@ -4,11 +4,40 @@ import config from '../../config/environment';
 
 export default Ember.Controller.extend({
 	activeAccount: config.APP.api.activeAccout,
-	account: {'Equity': 'NA', 'pnl': 'NA', 'funds': 'NA', 'margin': 'NA', 'avaliable': 'NA'},
+	account: {
+		'EQUITY': 'NA',
+		'PNL': 'NA',
+		'FUNDS': 'NA',
+		'MARGIN': 'NA',
+		'AVAILABLE_TO_DEAL': 'NA'
+	},
 
 	actions: {
 		getAcountDetails() {
-
+			console.log('init acc details');
+			const that = this;
+			const fields = ['PNL', 'EQUITY', 'FUNDS', 'MARGIN', 'AVAILABLE_TO_DEAL'];
+			const accountID = `ACCOUNT:${this.get('activeAccount')}`;
+			var subscription = new Lightstreamer.Subscription(
+				"MERGE", accountID, fields
+			);
+			subscription.addListener({
+				onSubscription: function() {
+					console.log('subscribed');
+				},
+				onUnsubscription: function() {
+					console.log('unsubscribed');
+				},
+				onSubscriptionError: function(code, message) {
+					console.log('subscription failure: ' + code + " message: " + message);
+				},
+				onItemUpdate: function(updateInfo) {
+					updateInfo.forEachField(function(fieldName, fieldPos, value) {
+						Ember.set(that.get('account'), fieldName, value);
+					});
+				}
+			});
+			config.APP.api.lsClient.subscribe(subscription);
 		}
 	}
 });
--- app/routes/search/account.js
+++ app/routes/search/account.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	setupController(controller, model) {
+		this._super(controller, model);
+		var controller = this.controllerFor('search/account');
+		controller.send('getAcountDetails');
+	},
 });
